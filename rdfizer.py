50e3f7689001f2b7547c20b1a6009186e0f41da8
Patryk Hubar <38289522+patthub@users.noreply.github.com>
2024-06-25 20:20:05 +0200

    Update rdfizer.py

--- rdfizer.py
+++ rdfizer.py
@@ -1,16 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 25 17:19:21 2024
-
-@author: patry
-"""
-
 import csv
 from rdflib import Graph, Namespace, URIRef, Literal
 from rdflib.namespace import RDF, RDFS, SKOS
 import os
 
-class RDFGraphFactory:
+class RDFGraphBuilder:
     def __init__(self):
         self.graph = Graph()
         self.namespace_manager()
@@ -21,29 +14,38 @@ class RDFGraphFactory:
         self.graph.bind("schema", SCHEMA)
         self.graph.bind("bibframe", BIBFRAME)
         self.graph.bind("skos", SKOS)
+        return self
 
-    def create_graph_from_csv(self, csv_file_path):
+    def add_triple(self, subject, predicate, obj):
+        subject = URIRef(subject)
+        predicate = URIRef(predicate)
+        obj = Literal(obj)
+        self.graph.add((subject, predicate, obj))
+        return self
+
+    def add_triples_from_csv(self, csv_file_path):
         with open(csv_file_path, 'r', encoding='utf-8') as csvfile:
             csvreader = csv.DictReader(csvfile)
             for row in csvreader:
-                subject = URIRef(row['subject'])
-                predicate = URIRef(row['predicate'])
-                obj = Literal(row['object'])
-                self.graph.add((subject, predicate, obj))
+                self.add_triple(row['subject'], row['predicate'], row['object'])
+        return self
 
-    def save_graph(self, file_path):
-        self.graph.serialize(destination=file_path, format='turtle')
+    def build(self):
+        return self.graph
 
 class RDFGraphClient:
-    def __init__(self, factory):
-        self.factory = factory
+    def __init__(self, builder):
+        self.builder = builder
 
     def create_and_save_graph(self, csv_file_path, output_file_path):
-        self.factory.create_graph_from_csv(csv_file_path)
-        self.factory.save_graph(output_file_path)
+        graph = self.builder.add_triples_from_csv(csv_file_path).build()
+        graph.serialize(destination=output_file_path, format='turtle')
 
 if __name__ == "__main__":
+    
     csv_file_path = 'example.csv'
-    factory = RDFGraphFactory()
-    client = RDFGraphClient(factory)
+    builder = RDFGraphBuilder()
+    client = RDFGraphClient(builder)
     client.create_and_save_graph(csv_file_path, 'output.ttl')
+
+
